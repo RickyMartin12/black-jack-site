f587a558cf22e021ce9ae650b4cb3e707cc535b8
https://github.com/RickyMartin12/moodle_elearning.git <11knum15@gmail.com>
2023-02-20 19:19:38 +0000

    1121

--- captcha.php
+++ captcha.php
@@ -4,48 +4,6 @@
 // the captcha externally!
 session_start();
 
-function create_capacha($text)
-{
-
-	$width = 200;
-	$height = 100;
-	$fontfile = "OpenSans-Regular.ttf";
-
-	$image = imagecreatetruecolor($width, $height);
-
-	$white = imagecolorallocate($image, 255, 255, 255);
-	$black = imagecolorallocate($image, 0, 0, 0);
-
-	imagefill($image, 0, 0, $white);
-	imagettftext($image, 25, rand(-20,20), $width/4, 60, $black, $fontfile, $text);
-
-	$warped_image = imagecreatetruecolor($width, $height);
-	imagefill($warped_image, 0, 0, imagecolorallocate($warped_image, 255, 255, 255));
-
-	for ($x=0; $x < $width; $x++) { 
-		# code...
-		for ($y=0; $y < $height; $y++) { 
-			# code...
-			$index = imagecolorat($image, $x, $y);
-			$color_comp = imagecolorsforindex($image, $index);
-
-			$color = imagecolorallocate($warped_image, $color_comp['red'], $color_comp['green'], $color_comp['blue']);
-
-			$imageX = $x;
-			$imageY = $y + sin($x / 10) * 10;
-
-			imagesetpixel($warped_image, $imageX, $imageY, $color);
-		}
-	}
-
-	$path = "capacha.jpg";
-	imagejpeg($warped_image,$path);
-	imagedestroy($warped_image);
-	imagedestroy($image);
-
-	return $path;
-}
-
 $filename = session_id();
 
 $captcha = rand(1000, 9999);
@@ -66,7 +24,7 @@ switch ($_GET['teste'])
 	$black = imagecolorallocate($image, 0, 0, 0);
 
         imagefill($image, 0, 0, $white);
-	imagettftext($image, 25, rand(-20,20), $width/4, 60, $black, $fontfile, $captcha);
+	imagettftext($image, 25, rand(-20,20), $width/4, 60, $black, $fontfile, $_SESSION["captcha"]);
 
         header('Content-type: image/jpeg');
 
@@ -77,8 +35,8 @@ switch ($_GET['teste'])
         break;
 
     case 'return_captcha':
-        
-        return $captcha;
+
+        return $_SESSION["captcha"];
 
         break;
 
