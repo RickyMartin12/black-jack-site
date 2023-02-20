8e7997e17ef4a72b3213ca16e7f8bb2c8a6d7c65
https://github.com/RickyMartin12/moodle_elearning.git <11knum15@gmail.com>
2023-02-20 17:40:49 +0000

    perm_image_zero

--- captcha.php
+++ captcha.php
@@ -4,46 +4,19 @@
 // the captcha externally!
 session_start();
 
+$filename = session_id();
+
 // Generate a random number
 // from 1000-9999
 switch ($_GET['teste'])
 {
     case 'image_captcha':
-        // The captcha will be stored
-        // for the session
-        // Generate a 50x24 standard captcha image
-        $im = imagecreatetruecolor(50, 24);
 
         $captcha = rand(1000, 9999);
         $_SESSION["captcha"] = $captcha;
 
-// Blue color
-        $bg = imagecolorallocate($im, 22, 86, 165);
-
-// White color
-        $fg = imagecolorallocate($im, 255, 255, 255);
-
-// Give the image a blue background
-        imagefill($im, 0, 0, $bg);
-
-// Print the captcha text in the image
-// with random position & size
-        imagestring($im, rand(1, 7), rand(1, 7),
-            rand(1, 7),  $captcha, $fg);
-
-// VERY IMPORTANT: Prevent any Browser Cache!!
-        header("Cache-Control: no-store,
-            no-cache, must-revalidate");
-
-// The PHP-file will be rendered as image
-        header('Content-type: image/png');
-
-// Finally output the captcha as
-// PNG image the browser
-        imagepng($im);
-
-// Free memory
-        imagedestroy($im);
+        $myimage = create_capacha($captcha);
+	file_put_contents($filename, $captcha);
         break;
 
     case 'return_captcha':
